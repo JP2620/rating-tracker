1d64c514505db209e8af8507bfd56bbf1fc6d4ba
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-02-09 01:00:39 -0300

    Creates window

---
+++ App.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+
+
+class App(ttk.Window):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master)
+        self
---
+++ main.py
@@ -0,0 +1,5 @@
+from App import App
+
+my_app = App(themename='journal')
+my_app.mainloop()
+
