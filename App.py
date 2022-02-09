f7de7eeb79f2af4d6d2a6c11469d963a2a21e387
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-02-09 01:50:48 -0300

    Added player form

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+from PlayerForm import PlayerForm
 import tkinter as tk
 import ttkbootstrap as ttk
 import json
@@ -5,7 +6,7 @@ import json
 
 class App(ttk.Window):
     def __init__(self, master=None, **kwargs):
-        super().__init__(master)
+        super().__init__(master, **kwargs)
         try:
           with open('conf.json', 'r') as f:
               config = json.load(f)
@@ -19,8 +20,13 @@ class App(ttk.Window):
         self.resizable(False, False)
         self.title("Rating Tracker")
         self.set_icon('images/Table-Tennis-1.png')
+        self.create_widgets()
 
 
     def set_icon(self, icon_path: str) -> None:
       self.iconphoto(False, tk.PhotoImage(file=icon_path))
       return
+    
+    def create_widgets(self) -> None:
+      self.player_form = PlayerForm(self, lambda: print("Jugador agregado"), text="Agregar jugador")
+      self.player_form.grid(row=0, column=0, sticky="EW")
