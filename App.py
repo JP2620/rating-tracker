eafa713f8a02ee137f15d05639fa7b14f0afeb2f
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-02-09 12:39:56 -0300

    Creates database

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ from PlayerForm import PlayerForm
 from MatchForm import MatchForm
 from DataView import DataView
 import tkinter as tk
+import sqlite3 as sql
 import ttkbootstrap as ttk
 import json
 
@@ -22,7 +23,10 @@ class App(ttk.Window):
         self.resizable(False, False)
         self.title("Rating Tracker")
         self.set_icon('images/Table-Tennis-1.png')
+        self.conn = self.create_database()
         self.create_widgets()
+
+        
 
 
     def set_icon(self, icon_path: str) -> None:
@@ -36,3 +40,49 @@ class App(ttk.Window):
       self.match_form.grid(row=0, column=0, sticky="EW")
       self.player_form.grid(row=1, column=0, sticky="EW")
       self.data_view.grid(row=2, column=0, sticky="EW")
+    
+    def create_database(self) -> sql.Connection:
+      
+      conn = None
+      try:
+        conn = sql.connect('torneo.db')
+        cur = conn.cursor()
+      except Error as e:
+        print(e)
+        exit()
+
+      cur.execute('''
+        CREATE TABLE IF NOT EXISTS Player (
+          PlayerId INTEGER PRIMARY KEY AUTOINCREMENT,
+          Name TEXT NOT NULL,
+          Rating INTEGER NOT NULL
+        ); '''
+      )
+
+      cur.execute('''
+        CREATE TABLE IF NOT EXISTS Match (
+          MatchId INTEGER PRIMARY KEY AUTOINCREMENT,
+          Player1Id INTEGER NOT NULL,
+          Player2Id INTEGER NOT NULL,
+          Player1Score INTEGER NOT NULL,
+          Player2Score INTEGER NOT NULL,
+          Date TEXT NOT NULL,
+          FOREIGN KEY(Player1Id) REFERENCES Player(PlayerId),
+          FOREIGN KEY(Player2Id) REFERENCES Player(PlayerId)
+        ); '''
+      )
+
+      cur.execute(
+        '''
+        INSERT INTO Player (Name, Rating)
+        VALUES ('Player 1', 1000),
+               ('Player 2', 900),
+               ('Player 3', 800)
+        '''
+      )
+      conn.commit()
+      cur.close()
+      return conn
+
+
+
